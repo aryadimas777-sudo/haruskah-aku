29131cddc88b898beda4b87156e38c1a28cecd47
asdwdev <asdwdev@gmail.com>
2026-03-22 19:06:13 +0700

    logic random jawaban

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+// function random jawaban
+func Jawab() string {
+	// list pilihan
+	jawaban := []string{"ya", "tidak"}
+
+	// random index -> ambil angka random dari 0 sampai panjang slice - 1
+	index := rand.Intn(len(jawaban))
+
+	return jawaban[index]
+}
 
 func main() {
-	fmt.Println("project haruskah aku dimulai")
+	// biar random beda tiap run
+	rand.Seed(time.Now().UnixNano())
+
+	fmt.Println("jawaban:", Jawab())
 }
